03f9804cc376d5a07e9cee7d4dbbb22d7465ec85
Swapneel Chaudhuri <113018040+swapneel111@users.noreply.github.com>
2024-07-29 13:40:11 +0000

    utc commit

--- components/forms/AppointmentForm.tsx
+++ components/forms/AppointmentForm.tsx
@@ -101,6 +101,7 @@ export const AppointmentForm = ({
             cancellationReason: values.cancellationReason,
           },
           type,
+          timeZone: 'UTC', // Replace with actual time zone value if available
         };
 
         const updatedAppointment = await updateAppointment(appointmentToUpdate);
@@ -125,7 +126,7 @@ export const AppointmentForm = ({
       buttonLabel = "Schedule Appointment";
       break;
     default:
-      buttonLabel = "Submit Apppointment";
+      buttonLabel = "Submit Appointment";
   }
 
   return (
